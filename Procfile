61a81093f2f3a3f350ff2964dac42463416525f0
Michal Jemala <mjemala@mj.local>
2014-11-02 22:15:46 +0000

    Rewritten to use the offical buildpack

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: hello-go
--- web.go
+++ web.go
@@ -3,52 +3,27 @@ package main
 import (
 	"code.google.com/p/log4go"
 	"fmt"
-	"launchpad.net/goyaml"
 	"net/http"
 	"os"
-	"runtime"
 )
 
-const (
-	HostVar = "VCAP_APP_HOST"
-	PortVar = "VCAP_APP_PORT"
-)
-
-type T struct {
-	A string
-	B []int
-}
+const Port = "VCAP_APP_PORT"
 
 func main() {
 	log := make(log4go.Logger)
 	log.AddFilter("stdout", log4go.DEBUG, log4go.NewConsoleLogWriter())
 
-	http.HandleFunc("/", hello)
-	var port string
-	if port = os.Getenv(PortVar); port == "" {
-		port = "8080"
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "<h1>Listening on %s</h1>", os.Getenv(Port))
+	})
+	
+	var p string
+	if p = os.Getenv(Port); p == "" {
+		p = "8080"
 	}
-	log.Debug("Listening at port %v\n", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	
+	log.Debug("Listening at port %v\n", p)
+	if err := http.ListenAndServe(":"+p, nil); err != nil {
 		panic(err)
-	}
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	// Dump Go version
-	fmt.Fprintf(res, "%v\n\n", runtime.Version())
-
-	// Dump ENV
-	env := os.Environ()
-	for _, e := range env {
-		fmt.Fprintln(res, e)
-	}
-
-	//Dump some YAML
-	t := T{A: "Foo", B: []int{1, 2, 3}}
-	if d, err := goyaml.Marshal(&t); err != nil {
-		fmt.Fprintf(res, "Unable to dump YAML")
-	} else {
-		fmt.Fprintf(res, "\n\n--- \n%s", d)
 	}
 }
