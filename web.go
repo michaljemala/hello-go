3354299450712a2a54c8e866fb8ecdbe1d6adeaf
Michal Jemala <michal.jemala@gmail.com>
2017-07-20 17:02:01 +0200

    Revert

--- web.go
+++ web.go
@@ -3,13 +3,53 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"runtime"
+
+	"code.google.com/p/log4go"
+	"launchpad.net/goyaml"
 )
 
+const (
+	HostVar = "VCAP_APP_HOST"
+	PortVar = "VCAP_APP_PORT"
+)
+
+type T struct {
+	A string
+	B []int
+}
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "OK")
-	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log := make(log4go.Logger)
+	log.AddFilter("stdout", log4go.DEBUG, log4go.NewConsoleLogWriter())
+
+	http.HandleFunc("/", hello)
+	var port string
+	if port = os.Getenv(PortVar); port == "" {
+		port = "8080"
+	}
+	log.Debug("Listening at port %v\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	// Dump Go version
+	fmt.Fprintf(res, "%v\n\n", runtime.Version())
+
+	// Dump ENV
+	env := os.Environ()
+	for _, e := range env {
+		fmt.Fprintln(res, e)
+	}
+
+	//Dump some YAML
+	t := T{A: "Foo", B: []int{1, 2, 3}}
+	if d, err := goyaml.Marshal(&t); err != nil {
+		fmt.Fprintf(res, "Unable to dump YAML")
+	} else {
+		fmt.Fprintf(res, "\n\n--- \n%s", d)
 	}
 }
